67bc0a674a83d51a4dba9b04d0c57698c8b09c98
Nicky Lenaers <nicky@nite-dev.nl>
2017-07-20 15:55:00 +0200

    Export Interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
 export { ScrollToModule } from './scroll-to.module';
+export { ScrollToEvent } from './models/scroll-to-event.model';
+export { ScrollToAnimationEasing } from './models/scroll-to-easing.model';
+export { ScrollToOffsetMap } from './models/scroll-to-options.model';
