b0dcaf7ddbb78296acf1efac176b7600796869e7
Nicky Lenaers <nicky@nite-dev.nl>
2017-10-03 21:57:08 +0200

    Update Indenting

--- src/app/modules/scroll-to/scroll-to.service.ts
+++ src/app/modules/scroll-to/scroll-to.service.ts
@@ -37,9 +37,9 @@ export class ScrollToService {
 	 * @todo type 'any' in Observable should become custom type like 'ScrollToEvent' (base class), see issue comment:
 	 * 	- https://github.com/nicky-lenaers/ngx-scroll-to/issues/10#issuecomment-317198481
 	 *
-	 * @param event 				Native Browser Event
-	 * @param config 				Configuration Object
-	 * @returns 					Observable
+	 * @param event         Native Browser Event
+	 * @param config        Configuration Object
+	 * @returns             Observable
 	 */
   @TimeOut()
   public scrollTo(event: any, config: ScrollToConfig): Observable<any> {
@@ -54,9 +54,9 @@ export class ScrollToService {
 	 *
 	 * @todo Emit proper events from subscription
 	 *
-	 * @param event 				Native Browser Event
-	 * @param config 				Configuration Object
-	 * @returns 					Observable
+	 * @param event         Native Browser Event
+	 * @param config        Configuration Object
+	 * @returns             Observable
 	 */
   private _start(event: any, config: ScrollToConfig): Observable<number> {
 
@@ -99,10 +99,10 @@ export class ScrollToService {
 	/**
 	 * Add listeners for the Animation Stop Event.
 	 *
-	 * @param events 					List of events to listen to
-	 * @param listenerTarget 			Target to attach the listener on
-	 * @param handler 					Handler for when the listener fires
-	 * @returns void
+	 * @param events            List of events to listen to
+	 * @param listenerTarget    Target to attach the listener on
+	 * @param handler           Handler for when the listener fires
+	 * @returns                 void
 	 */
   private _addStopEventListeners(events: string[], listenerTarget: ScrollToListenerTarget, handler: EventListenerOrEventListenerObject): void {
     events.forEach(event => listenerTarget.addEventListener(event, handler));
@@ -111,10 +111,10 @@ export class ScrollToService {
 	/**
 	 * Remove listeners for the Animation Stop Event.
 	 *
-	 * @param events 					List of events to listen to
-	 * @param listenerTarget 			Target to attach the listener on
-	 * @param handler 					Handler for when the listener fires
-	 * @returns void
+	 * @param events            List of events to listen to
+	 * @param listenerTarget    Target to attach the listener on
+	 * @param handler           Handler for when the listener fires
+	 * @returns                 void
 	 */
   private _removeStopEventListeners(events: string[], listenerTarget: ScrollToListenerTarget, handler: EventListenerOrEventListenerObject): void {
     events.forEach(event => listenerTarget.removeEventListener(event, handler));
@@ -123,8 +123,8 @@ export class ScrollToService {
 	/**
 	 * Find the first scrollable parent node of an element.
 	 *
-	 * @param nativeElement 			The element to search from
-	 * @return 							The first scrollable parent element
+	 * @param nativeElement     The element to search from
+	 * @return                  The first scrollable parent element
 	 */
   private _getFirstScrollableParent(nativeElement: HTMLElement): HTMLElement {
 
@@ -159,8 +159,8 @@ export class ScrollToService {
 	/**
 	 * Get the Target Node to scroll to.
 	 *
-	 * @param id 			The given ID of the node, either a string or an element reference
-	 * @returns 			Target Node
+	 * @param id          The given ID of the node, either a string or an element reference
+	 * @returns           Target Node
 	 */
   private _getTargetNode(id: ScrollToTarget): HTMLElement {
 
@@ -189,8 +189,8 @@ export class ScrollToService {
 	/**
 	 * Retrieve the Listener target.
 	 *
-	 * @param container 				The HTML Container element
-	 * @returns 						Listener
+	 * @param container           The HTML Container element
+	 * @returns                   Listener
 	 */
   private _getListenerTarget(container: HTMLElement): ScrollToListenerTarget {
     return container.tagName.toUpperCase() === 'BODY' ? window : container;
