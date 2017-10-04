84174bf0ed76b9e1679ed301e993d3a5ceef9816
Nicky Lenaers <nicky@nite-dev.nl>
2017-10-04 10:01:44 +0200

    CSS to Sass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app';
