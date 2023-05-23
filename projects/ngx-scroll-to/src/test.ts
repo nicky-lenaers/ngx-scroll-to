f85e6fad5709902585ca7e91bf93a19431c8df25
Mickaël Depardon <mickael.depardon@wizbii.com>
2023-05-23 15:24:06 +0200

    chore: update to Angular 15

--- projects/ngx-scroll-to/src/test.ts
+++ projects/ngx-scroll-to/src/test.ts
@@ -8,13 +8,6 @@ import {
   platformBrowserDynamicTesting
 } from '@angular/platform-browser-dynamic/testing';
 
-declare const require: {
-  context(path: string, deep?: boolean, filter?: RegExp): {
-    <T>(id: string): T;
-    keys(): string[];
-  };
-};
-
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
@@ -23,8 +16,3 @@ getTestBed().initTestEnvironment(
     teardown: { destroyAfterEach: false }
   }
 );
-
-// Then we find all the tests.
-const context = require.context('./', true, /\.spec\.ts$/);
-// And load the modules.
-context.keys().forEach(context);
